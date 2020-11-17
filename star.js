2dca259d23a884e05c59a45b841dc303bc86bc91
teapuddles <kingsley.gleeson@gmail.com>
2020-11-17 17:46:11 -0500

    added code to work on into star.js

---
+++ star.js
@@ -0,0 +1,58 @@
+//  function renderOneConstell(constell) {
+//   const showOne = document.querySelector("#star-detail")
+
+//   showOne.innerHTML =
+//     `<img src="${constell.image_url}" alt="${constell.name}">
+//       <h1>${constell.name}</h1>
+//       <h3>Season: ${constell.season.name}</h3>
+//        <div class="likes">
+//        <span class="likeCount">${constell.likes.length}</span>  Likes
+//       </div>
+//       <button data-action="like" data-constellation_id="${constell.id}" class="like-btn">Like <3</button></div>
+//       </div>
+      
+//       <p class="description">
+//          ${constell.description}
+//      </p>`
+//   // like button logic/create likes
+//   const likeButt = document.querySelector(".like-btn")
+//   likeButt.addEventListener('click', () => {
+
+//     const newLike = {
+//       constellation_id: constell.id,
+//       user_id: localStorage.id
+//       // local storage is holding my user id so I can create a like.
+//     }
+
+//     fetch( `https://east-side-stargazing-be.herokuapp.com/likes`, {
+//       method: 'POST',
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify(newLike)
+//     })
+//       .then(r => r.json())
+//       .then(constell => renderOneConstell(constell))
+//   })
+//   // const h1 = showOne.querySelector("h1")
+//   // h1.addEventListener('click', () => reverseFunction(constell))
+
+//   // function reverseFunction(constell) {
+//   //  const constellName = {
+//   //     name: constell.name 
+//   //   }
+
+//   //   fetch('localhost:3000/constellations/${constell.id}/reverse', {
+//   //     method: "PATCH",
+//   //     headers: {
+//   //       "Content-Type": "application/json"
+//   //     },
+//   //     body: JSON.stringify(constellName)
+//   //   })
+//   //   .then(r => r.json())
+//   //   .then(revName => {
+//   //     h1.innerText = revName
+//   //   })
+//   // }
+
+// }
